efe296103a212c2ebf5e5997a4e7d462f681f309
luozhanhong <937316110@qq.com>
2018-06-16 22:46:27 +0800

    promiseTest

--- leetcode/0.01.js
+++ leetcode/0.01.js
@@ -16,3 +16,33 @@ new Promise(function (resoler, reject) {
   console.log(3);
 });
 console.log(4);
+
+async function promiseTest() {
+  let p1 = function () {
+    return new Promise(function (resoler, reject) {
+      resoler('this is success');
+    }).then(d => {
+      return 'this is then 1 success';
+    }).then(d => {
+      console.log('then 2: ', d);
+      return 'this is then 2 success';
+    });
+  };
+  let p2 = function () {
+    return new Promise(function (resoler, reject) {
+      throw new Error('this is throw error');
+      // reject(new Error('this is reject error'));
+    }).then(d => {
+      return 'this is then success';
+    }).catch(err => {
+      return err;
+    });
+  };
+
+  console.log('p1:', await p1());
+  console.log('p2:', await p2());
+}
+promiseTest();
+// then 2:  this is then 1 success
+// p1: this is then 2 success
+// p2: Error: this is throw error
---
+++ leetcode/006-ZigZag Conversion.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function (s, numRows) {
+  let str=[[]];
+  for (let i = 0; i < s.length; i++) {
+
+  }
+};
+console.log(convert("PAYPALISHIRING", 3));
+console.log(convert("PAYPALISHIRING", 4));
